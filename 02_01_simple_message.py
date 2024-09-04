912bc34c360b3b009860c7a3063b85c8ea725c3c
Joshua-Montilla <joshuagaming0721@gmail.com>
2024-09-04 09:25:28 -0400

    commit

---
+++ 02_01_simple_message.py
@@ -0,0 +1,11 @@
+message = "Hello how are you doing? How's your day going?"
+
+print(message)
+
+#message = "Hello Python Crash Course World!"
+
+#print(message)
+
+#mesage = "Hello Python Crash Course Reader!"
+
+#print(mesage)
---
+++ 02_02_simple_message.py
@@ -0,0 +1,7 @@
+#message = "Hello how are you doing? How's your day going?"
+
+#print(message)
+
+message = "You're doing great? That's great to hear!"
+
+print(message)
