81bcc650ee373f2b240388b62712545ee215390b
Joshua-Montilla <joshuagaming0721@gmail.com>
2024-09-05 09:15:58 -0400

    commit

--- 02_variables_and_data_Types/02_07_stripping_names.py
+++ 02_variables_and_data_Types/02_07_stripping_names.py
@@ -1,3 +1,9 @@
+
+
+
+
+import this # Skimming through this so far has been rather strange and pretty funny. It seems to be meant to be confusing and completely throw off anyone looking into it seriously. A joke of python very nice to look into.
+
 name = "                      Marin Morvayne                            "
 
 print(name.lstrip() + "\t")
